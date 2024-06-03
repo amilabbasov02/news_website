e065bc91dbf6300d5b57a79ce8145788dbdaaf06
amilabbasov02 <amilabbas451.19@gmail.com>
2024-06-03 19:12:30 +0400

    manshet block end

--- js/main.js
+++ js/main.js
@@ -9,4 +9,18 @@ $('.owl-carousel').owlCarousel({
     margin:10,
     nav:true,
     items: 1,
+    dots: false,
+    navText: ["<img src='./img/arrow_left.png'>", "<img src='./img/arrow_right.png'>"]
+})
+$(".select_list a").click(function () {
+    $(".select_list a").removeClass("active")
+    $(this).addClass("active")
+    if ($('.news_lent').hasClass('active')) {
+        $('.lent_news_block').css('display', 'block');
+        $('.top_news_block').css('display', 'none');
+    }
+    if ($('.top_news').hasClass('active')) {
+        $('.lent_news_block').css('display', 'none');
+        $('.top_news_block').css('display', 'block');
+    }
 })
